89a78b760b7421e09189c88152108a7719a9326d
Adielson-Ferreira <adielson.ferreira19@gmail.com>
2022-01-12 16:32:34 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as GithubIcon} from 'assets/img/Vector.svg';
+import './styles.css';
+function Navbar() {
+    return (
+        <header>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/Adielson-Ferreira" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/Adielson-Ferreira</p>
+                </div>
+            </a>
+        </div>
+    </nav>
+</header>
+    );
+}
+export default Navbar;
